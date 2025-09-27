3d314e8d890fccc3df8ca20249d927c6ff5a88b5
Dylan-King07 <dylan.king1@my.jcu.edu.au>
2025-09-27 17:24:41 +1000

    Refactored password program to have multiple functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,11 +1,17 @@
 # Main function
 def main():
+    password = get_password()
+    print_password_asterisks(password)
+
+def get_password() -> str:
     minimum_length = int(input("Enter minimum character limit: "))
     password = input(f"Input password with at least {minimum_length} characters: ")
     while len(password) < minimum_length:
         print("Password too short")
         password = input(f"Input password with at least {minimum_length} characters: ")
-    print(f"Password: {'*' * len(password)}")
     return password
+
+def print_password_asterisks(password: str):
+    print(f"Password: {'*' * len(password)}")
 
 main()
