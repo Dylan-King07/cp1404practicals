a2e9dec79e4d709b41deb19544cd456cb0cb7705
Dylan-King07 <dylan.king1@my.jcu.edu.au>
2025-10-11 17:13:18 +1000

    Complete code for subject_reader.py to perform in line with prac instruction

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,28 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    """Read from file and display subject details"""
+    subjects = load_subject_data(FILENAME)
+    display_subject_details(subjects)
 
 
-def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
+def load_subject_data(filename=FILENAME):
+    """Load data from file and return list [code, learner, student_count]"""
+    subjects = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        subjects.append(parts)
     input_file.close()
+    return subjects
+
+
+def display_subject_details(subjects):
+    """Display formatted subject details"""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 
 main()
