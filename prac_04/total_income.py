0638c3c88fee0deec5824cfe020d02f986e6d9f5
Dylan-King07 <dylan.king1@my.jcu.edu.au>
2025-10-12 15:12:59 +1000

    Final change to total_income.py before submission

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -12,11 +12,11 @@ def main():
     for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for {month}: "))
         incomes.append(income)
-    print_income_report(incomes)
+    print_report(incomes)
 
 
-def print_income_report(incomes):
-    """Display income report"""
+def print_report(incomes):
+    """Print income report"""
     print("\nIncome Report\n-------------")
     total = 0
     for month, income in enumerate(incomes, start=1):
