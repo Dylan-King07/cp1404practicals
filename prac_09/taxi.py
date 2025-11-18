c3a70c904b870953ab971879e62852c1cec9bf72
Dylan-King07 <dylan.king1@my.jcu.edu.au>
2025-11-19 09:40:36 +1000

    Add taxi.py to prac_09 repository (wrong initial commit of taxi.py)

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -1,37 +1,33 @@
-"""CP1404/CP5632 Practical - Car class example."""
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+from prac_09.car import Car
 
 
-class Car:
-    """Represent a Car object."""
+class Taxi(Car):
+    """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name="Car", fuel=0):
-        """Initialise a Car instance.
-
-        name: string, reference name for car
-        fuel: float, one unit of fuel drives one kilometre
-        """
-        self.name = name
-        self.fuel = fuel
-        self._odometer = 0
+    def __init__(self, name, fuel, price_per_km):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.price_per_km = price_per_km
+        self.current_fare_distance = 0
 
     def __str__(self):
-        """Return a string which represents the Car."""
-        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
 
-    def add_fuel(self, amount):
-        """Add amount to the car's fuel."""
-        self.fuel += amount
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.price_per_km * self.current_fare_distance
+
+    def start_fare(self):
+        """Begin a new fare."""
+        self.current_fare_distance = 0
 
     def drive(self, distance):
-        """Drive the car a given distance.
-
-        Drive given distance if car has enough fuel
-        or drive until fuel runs out return the distance actually driven.
-        """
-        if distance > self.fuel:
-            distance = self.fuel
-            self.fuel = 0
-        else:
-            self.fuel -= distance
-        self._odometer += distance
-        return distance
+        """Drive like parent Car but calculate fare distance as well."""
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
